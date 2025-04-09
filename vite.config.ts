393e86e88a138cee7df852f73183b31d60092fa4
Clement Masauko <bsc-com-ne-04-20@uniman.ac.mw>
2025-04-09 22:38:20 +0200

    vite edits

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@components': path.resolve(__dirname, './src/components')
-    }
+    },
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json']  // Add this
   }
-});
+})
